40f99530ff50d9fc3e18e7dddf49ea44e34f02cc
Carlos Tojal <carlos.tojal@hotmail.com>
2023-04-27 22:59:45 +0100

    create point class

---
+++ include/Point.h
@@ -0,0 +1,38 @@
+//
+// Created by carlostojal on 27/04/2023.
+//
+
+#ifndef DELAUNAY_PATH_PLANNER_CORE_POINT_H
+#define DELAUNAY_PATH_PLANNER_CORE_POINT_H
+
+#include <stdexcept>
+
+namespace path_planner {
+
+    class Point {
+
+        private:
+            double x;
+            double y;
+            double theta;
+
+            bool xSet = false;
+            bool ySet = false;
+            bool thetaSet = false;
+
+        public:
+            Point();
+
+            double getX() const;
+            void setX(double x);
+
+            double getY() const;
+            void setY(double y);
+
+            double getTheta() const;
+            void setTheta(double theta);
+    };
+
+} // path_planner
+
+#endif //DELAUNAY_PATH_PLANNER_CORE_POINT_H
---
+++ src/Point.cpp
@@ -0,0 +1,44 @@
+//
+// Created by carlostojal on 27/04/2023.
+//
+
+#include "../include/Point.h"
+
+namespace path_planner {
+
+    double Point::getX() const {
+        if(!this->xSet)
+            throw std::runtime_error("Point x value not set!");
+
+        return x;
+    }
+
+    void Point::setX(double x) {
+        this->xSet = true;
+        this->x = x;
+    }
+
+    double Point::getY() const {
+        if(!this->ySet)
+            throw std::runtime_error("Point y value not set!");
+
+        return y;
+    }
+
+    void Point::setY(double y) {
+        this->ySet = true;
+        this->y = y;
+    }
+
+    double Point::getTheta() const {
+        if(!this->thetaSet)
+            throw std::runtime_error("Point theta value not set!");
+
+        return theta;
+    }
+
+    void Point::setTheta(double theta) {
+        this->thetaSet = true;
+        this->theta = theta;
+    }
+} // path_planner
