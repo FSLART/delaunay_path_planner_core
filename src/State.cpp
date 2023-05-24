ebcc4a2a6cf69ff801038d046bcc318e2c5ea752
carlostojal <carlos.tojal@hotmail.com>
2023-05-24 23:17:04 +0100

    add yaw angle to state

--- src/State.cpp
+++ src/State.cpp
@@ -103,4 +103,12 @@ namespace path_planner {
             return UNKNOWN;
     }
 
+    double State::getYawAngle() const {
+        return yawAngle;
+    }
+
+    void State::setYawAngle(const float &yawAngle) {
+        this->yawAngle = yawAngle;
+    }
+
 } // path_planner
