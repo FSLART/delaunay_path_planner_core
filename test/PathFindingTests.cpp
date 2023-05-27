98478039da8b46a546521a0cf9bd1ceb3fc7b3ac
carlostojal <carlos.tojal@hotmail.com>
2023-05-27 19:43:19 +0100

    create boilerplate to add path finding tests

---
+++ test/PathFindingTests.cpp
@@ -0,0 +1,37 @@
+//
+// Created by carlostojal on 27-05-2023.
+//
+
+#include <gtest/gtest.h>
+#include <delaunay_path_planner_core/Environment.h>
+
+class PathFindingTests : public ::testing::Test {
+
+protected:
+
+    path_planner::Environment environment;
+
+    virtual void SetUp() {
+
+    }
+
+    virtual void TearDown() {
+
+    }
+
+};
+
+TEST_F(PathFindingTests, straightTrackSegmentPlanning) {
+
+    // TODO: test on a straight segment
+}
+
+TEST_F(PathFindingTests, curvedTrackSegmentPlanning) {
+
+    // TODO: test on a curve
+}
+
+TEST_F(GeometricTests, inconsistentTrackSegmentPlanning) {
+
+    // TODO: test on a segment with mis-colored cones
+}
